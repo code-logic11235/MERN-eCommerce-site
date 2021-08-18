2686d028cc2a749dffe80204dfcd13ca91549599
code-logic11235 <tai.pham1738@gmail.com>
2021-08-18 15:17:23 -0600

    created JWT

--- backend/controller/authUserController.js
+++ backend/controller/authUserController.js
@@ -19,9 +19,11 @@ exports.registerUser = catchAsyncErrors (async (req, res, next)=>{
       }
     })
 
+    const token = user.getJwtToken();
+
     res.status(201).json({
       success: true,
-      user
+      token
     })
   
   })
